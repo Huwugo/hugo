c6b5aa584f5888a5e4de1480c2828b7849057757
hugoy <hugo.yu@gmx.de>
2023-11-07 17:21:42 +0100

    hallo

---
+++ src/main/java/net/huwugo/modhugo/ModItems/ModItems.java
@@ -0,0 +1,24 @@
+package net.huwugo.modhugo.ModItems;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.huwugo.modhugo.ModHugo;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item FORK = registerItem("fork", new Item(new FabricItemSettings()));
+
+    public static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(FORK);
+    }
+
+    public static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(ModHugo.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        ModHugo.LOGGER.info("Registering mod items for" + ModHugo.MOD_ID);
+    }
+}
